a4e404b44776c4d94eff0363a085de99bd6f2aa6
kotasugya <no.shaking.throne974zero@gmail.com>
2020-12-11 00:18:52 +0900

    Final

--- slot.rb
+++ slot.rb
@@ -1,75 +1,90 @@
-# 仕様メモ
-# 入力をするごとにスロットの値がきまる（１セット３回入力）
-# 値が揃った場合はポイントを獲得できる
-# スロットを一回行う毎にコインを消費する
-# コインがある場合は「スロットを行う」または「やめる」を選択できる
-# コインがなくなった場合はスロットゲームを終了とする
-#https://app.gitbook.com/@potepan/s/rails-2/shi-jian-ke-ti/ruby
+$SLOT_NUMS = [1,2,3,4,5,6,7,8,9]
+$COINS = ["10", "20" "30"]
 
-# ーーーーーーーーーーーーーーーーーーーー
-# コインとポイントを定義する
-# コイン：100枚
-# ポイント：0
-# ※上記の増減は後で決める
-
-# 手持ちのコインとポイントを表示する
+# 手持ちコインとポイント
 def your_assets
-  your_coin = 100 - coins[selected_coin]
-  your_point = 0 #増えるように書き足す
+  @your_coin = 100 - $COINS[@selected_coin]
+  your_point = 0 + 
   puts "あなたの所持コインは#{your_coin}枚、ポイントは#{your_point}です"
+end
+
+# 選択肢の範囲内かチェックする
+def check_range(exact, from, to)
+  if from > exact || to < exact
+    puts "そんな選択肢はありません、選び直してください"
+    return true
+  end
+  return false
 end
 
 # 入れるコインを選択する(選択肢の範囲外は繰り返し、コインが足りなければゲームオーバー)
 def select_coin
   puts "入れるコインの枚数を選択してください"
   puts "0(10枚), 1(20枚), 2(30枚) 3(ゲームをやめる)"
-  selected_coin = gets.to_i
-  if  check_range(selected_coin, 0,3)
-    select_coin
-  elsif your_coin > select_coin
+  @selected_coin = gets.to_i
+  if check_range(@selected_coin, 0, 3)
+    return select_coin
+  elsif @your_coin < @selected_coin
     puts "コインが足りません、ゲームオーバーです"
     return false
   else
-    return  selected_coin
+    return  
   end
 end
 
 # 入れたコインの枚数を表示する、残りのコインを表示する
 def print_selected_coin
-  coins = ["10", "20" "30"]
   puts "------------------"
-  puts "コインを#{coins[selected_coin]}枚入れました。"
+  puts "コインを#{$COINS[selected_coin]}枚入れました。"
   puts "残りのコインは#{your_coin}枚です"
   puts "------------------"
 end
 
-# スロットを回す
-def spin_slot
-  slot_nums = [1,2,3,4,5,6,7,8,9]
+# スロットを回す_1回目
+def spin_slot_1
   puts "エンターを押してください"
   enter = gets
   if enter != ""
-  (0..2).each do |i|
-      slot_num = slot_nums[rand(slot_nums.length)]
-      puts "|#{slot_num}|"
-    end
+    @slot_num_1 = $SLOT_NUMS[rand($SLOT_NUMS.length)]
+    puts "|#{@slot_num_1}|"
   end
 end
 
-spin_slot
+# スロットを回す_2回目
+def spin_slot_2
+  puts "エンターを押してください"
+  enter = gets
+  if enter != ""
+    @slot_num_2 = $SLOT_NUMS[rand($SLOT_NUMS.length)]
+    puts "|#{@slot_num_1}|"+"|#{@slot_num_2}|"
+  end
+end
 
-# 必ず初回のスロットが回る
+# スロットを回す_3回目
+def spin_slot_3
+  puts "エンターを押してください"
+  enter = gets
+  if enter != ""
+    @slot_num_3 = $SLOT_NUMS[rand($SLOT_NUMS.length)]
+    puts "|#{@slot_num_1}|" + "|#{@slot_num_2}|" + "|#{@slot_num_3}|"
+  end
+end
 
 # スロットを回す
-# コインを入れると１列(1~9)の数字がランダムで決まる処理を作成する
-# これを３回繰り返し処理する
+def spin_slot
+  spin_slot_1
+  spin_slot_2
+  spin_slot_3
+end
 
-# # 結果の判断
-# 3列の数字が(横で)同じだった場合
-#   ポイントを獲得
-#   再度スロットを回すか判断
-# 3列の数字が揃わなかった場合
-#   再度スロットを回すか判断
-
-# # ポイントの倍率
-# 数字が揃った場合、入れたコインの倍のポイントを与える
+# 当たりの判定
+def check_bonus
+  @get_point = $COINS[@selected_coin]*2
+  if @slot_num_1 == @slot_num_2 == @slot_num_3
+    puts "おめでとうございます"
+    puts "#{@get_point}ポイント獲得しました"
+    return true
+  else
+    return true
+  end
+end
