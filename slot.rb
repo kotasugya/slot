b3294017aec3d63c030ab05c69e90abe3fffeac5
kotasugya <no.shaking.throne974zero@gmail.com>
2020-12-11 22:05:58 +0900

    Almost Finish slot_app

--- slot.rb
+++ slot.rb
@@ -1,11 +1,12 @@
 $SLOT_NUMS = [1,2,3,4,5,6,7,8,9]
-$COINS = ["10", "20" "30"]
+$COINS = [10, 20, 30]
 
 # 手持ちコインとポイント
 def your_assets
-  @your_coin = 100 - $COINS[@selected_coin]
-  @your_point = 0 + @get_point
+  @your_coin = 100
+  @your_point = 0
   puts "あなたの所持コインは#{@your_coin}枚、ポイントは#{@your_point}です"
+  return select_coin
 end
 
 # 選択肢の範囲内かチェックする
@@ -21,22 +22,26 @@ end
 def select_coin
   puts "入れるコインの枚数を選択してください"
   puts "0(10枚), 1(20枚), 2(30枚) 3(ゲームをやめる)"
-  @selected_coin = gets.to_i
-  if check_range(@selected_coin, 0, 3)
-    return select_coin
-  elsif @your_coin < $COINS[@selected_coin]
+  @selected_num = gets.to_i
+  @take_coin = $COINS[@selected_num]
+  if check_range(@selected_num, 0, 3)
+    select_coin
+  elsif @selected_num == 3
+    puts "しっぽを巻いて逃げ出した"
+    return false
+  elsif @your_coin < @take_coin
     puts "コインが足りません、ゲームオーバーです"
     return false
   else
-    return spin_slot
+    print_selected_coin
+    spin_slot_1
   end
 end
 
 # 入れたコインの枚数を表示する、残りのコインを表示する
 def print_selected_coin
   puts "------------------"
-  puts "コインを#{$COINS[@selected_coin]}枚入れました。"
-  your_assets
+  puts "コインを#{@take_coin}枚入れました。"
   puts "------------------"
 end
 
@@ -48,7 +53,7 @@ def spin_slot_1
     @slot_num_1 = $SLOT_NUMS[rand($SLOT_NUMS.length)]
     puts "|#{@slot_num_1}|"
   end
-  return spin_slot_2
+  spin_slot_2
 end
 
 # スロットを回す_2回目
@@ -59,7 +64,7 @@ def spin_slot_2
     @slot_num_2 = $SLOT_NUMS[rand($SLOT_NUMS.length)]
     puts "|#{@slot_num_1}|"+"|#{@slot_num_2}|"
   end
-  return spin_slot_3
+  spin_slot_3
 end
 
 # スロットを回す_3回目
@@ -70,33 +75,28 @@ def spin_slot_3
     @slot_num_3 = $SLOT_NUMS[rand($SLOT_NUMS.length)]
     puts "|#{@slot_num_1}|" + "|#{@slot_num_2}|" + "|#{@slot_num_3}|"
   end
-  return check_bonus
+  check_bonus
 end
 
 # スロットを回す
 def spin_slot
-  print_selected_coin
-  spin_slot_1
-  spin_slot_2
-  spin_slot_3
-
+  your_assets
 end
 
 # 当たりの判定
 def check_bonus
-  if @slot_num_1 == @slot_num_2 == @slot_num_3
-    @get_point = $COINS[@selected_coin]*2
+  if @slot_num_1 == @slot_num_2 && @slot_num_1 == @slot_num_3
+    @get_point = @take_coin * 2
     puts "おめでとうございます"
     puts "#{@get_point}ポイント獲得しました"
     return true
   else
-    your_assets
     return true
   end
 end
 
-next_slot = true
+next_game = true
 
-while next_slot do
-  next_slot = select_coin
+while next_game do
+  next_game = spin_slot
 end
